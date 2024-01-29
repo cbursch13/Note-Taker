6e930f8588e1bac25fecea91b36d03943af21d92
cammichbursc@gmail.com <cammichbursc@gmail.com>
2024-01-28 23:03:50 -0600

    updated file structure and updated get and post routes

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+var uniqid = require('uniqid');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'), (err) => {
+        if(err) throw err;
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    var note = req.body;
+    note.id = uniqid();
+
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if(err) throw err;
+        var currentFile = JSON.parse(data);
+
+        currentFile.push(note);
+    
+    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(currentFile), (err) => {
+        if(err) throw err;
+        console.log(`File saved: added note ID ${note.id}`);
+        });
+    });
+
+    res.sendFile(path.join(__dirname, './db/db.json'));
+  });
+
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
